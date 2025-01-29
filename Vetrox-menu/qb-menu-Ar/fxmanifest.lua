1a60698833282924975009a44886093178366dd7
vetrox12 <zmp56572@gmail.com>
2025-01-29 21:12:02 +0300

    Add files via upload

---
+++ Vetrox-menu/qb-menu-Ar/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Professor'
+description 'Vetrox-menu'
+version '1.0.0'
+
+client_script 'client/main.lua'
+
+ui_page 'html/index.html'
+
+files {
+    'html/*.*'
+}
+
+lua54 'yes'
